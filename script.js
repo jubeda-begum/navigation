685e23fcc2e1fe05797442ed0a0a65d91e41a3b7
jubeda-begum <shaikjubedabegum50@gmail.com>
2025-11-30 19:31:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const heading = document.getElementById("mainHeading");
+heading.textContent = "Welcome to the DOM World";
+heading.style.color = "blue";   
+const firstPara = document.getElementsByClassName("info")[0];
+firstPara.style.backgroundColor = "yellow";
+const boxDiv = document.querySelector(".box");
+boxDiv.style.backgroundColor = "lightgreen";
